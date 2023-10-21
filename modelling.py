32bf8e653d6350b9d72ade0c8b7680912a0179ab
Tim-Abwao <abwaomusungu@gmail.com>
2023-10-21 16:21:31 +0300

    Update streamlit app
    Improve appearance and cut cruft.

--- modelling.py
+++ modelling.py
@@ -3,10 +3,8 @@ from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-data = load_iris()
-X_train, X_test, y_train, y_test = train_test_split(
-    data.data, data.target, random_state=1
-)
+X, y = load_iris(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 
 if __name__ == "__main__":
     model = RandomForestClassifier(random_state=2)
